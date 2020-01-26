3e2790930a36c3fbec386f9ffc901b03ee1d0bb1
unknown <jai9399@gmail.com>
2020-01-27 01:15:46 +0530

    init

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ app.use(express.json());
 app.use(express.static(path.resolve('./statics')));
 const cookiep = require('cookie-parser');
 app.use(cookiep());
-app.use('/',router);
-app.use('/',router2);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+app.use('/',router);
+app.use('/',router2);
+
 app.get('/faculty',auth,function(req,res){
     faculties.find({}).then((users)=>{
         res.send(users);
