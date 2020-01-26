f8051a002c2df2afcf6780c86115b379c14e8039
unknown <jai9399@gmail.com>
2020-01-26 22:40:31 +0530

    Init

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL,{
+const url = process.env.MONGO_URL;
+mongoose.connect(url,{
     useCreateIndex:true,
     useFindAndModify:false,
     useNewUrlParser:true,
