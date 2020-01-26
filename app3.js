206374c59b442dda56f0f5f6357c8456ba04819a
unknown <jai9399@gmail.com>
2020-01-26 23:08:32 +0530

    Init

--- app3.js
+++ app3.js
@@ -35,7 +35,9 @@ router.post('/login',async function(req,res){
                }});}})})
 
 router.post('/signup',async function(req,res){
-    console.log(req);
+    if(req.body.email == '' || req.body.password == ''){
+        return res.send('Must Conatin Values')
+    }
     bcrypt.hash(req.body.password, 10, async function (err, hash) {
         if (err) {
         res.send(err);
