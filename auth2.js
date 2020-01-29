1990043755c8c0ee87b389c7bb56338dedc76ae2
unknown <jai9399@gmail.com>
2020-01-30 00:35:18 +0530

    Init

--- auth2.js
+++ auth2.js
@@ -1,10 +1,11 @@
 
 const faculties = require('./model/facinfo');
+const mongoose = require('mongoose')
 const existcomment = async function(req,res,next){
     const sentuser = req.cookies.user;
     let flag=0;
     let comment;
-    await faculties.findOne({id:req.params.id}).then(async (faculty)=>{
+    await faculties.findOne({_id:req.params.id}).then(async (faculty)=>{
            let comments = faculty.comments;
             await comments.forEach(element => {
                 if(element.email == sentuser.email){
