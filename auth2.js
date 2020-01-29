3e2b9d703847ba75046c855a09fe8407353877a6
unknown <jai9399@gmail.com>
2020-01-30 00:51:39 +0530

    Init

--- auth2.js
+++ auth2.js
@@ -5,6 +5,7 @@ const existcomment = async function(req,res,next){
     const sentuser = req.cookies.user;
     let flag=0;
     let comment;
+    console.log(req.params.id);
     await faculties.findOne({_id:req.params.id}).then(async (faculty)=>{
         console.log(faculty);
            let comments = faculty.comments;
