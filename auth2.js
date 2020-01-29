eada21b42e57057bac939ccdd74a42b1f0039b76
unknown <jai9399@gmail.com>
2020-01-30 01:11:11 +0530

    Init

--- auth2.js
+++ auth2.js
@@ -3,13 +3,12 @@ const faculties = require('./model/facinfo');
 const existcomment = async function(req,res,next){
     const sentuser = req.cookies.user;
     let flag=0;
-    let comment;
     console.log(req.params.id);
     await faculties.findOne({"_id":req.params.id}).then(async (faculty)=>{
         console.log(faculty);
            let comments = faculty.comments;
             comments.forEach(element => {
-                if(element.email == sentuser.email){
+                if(element.useremail == sentuser.email){
                     flag=1;
                     console.log('Found');
                 }    
